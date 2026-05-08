216858907dfe5e17e47d21e6e7af99f33bbbb172
joaquinpereira1993 <joaquinpereira40@gmail.com>
2026-05-07 21:18:08 -0300

    Clase mejorada

--- src/projectJava/Usuario.java
+++ src/projectJava/Usuario.java
@@ -1,9 +1,18 @@
 package projectJava;
 
 public class Usuario {
-     String nombre;
-     String apellido;
-     String email;
-     String contrasena;
+     public static void main(String[] args) {
+     String nombre = "Joaquin";
+     String apellido = "Pereira";
+     String email = "joaquinpereira@gmail.com";
+     String contrasena = "12345";
+     String pais = "Uruguay";
+
+          System.out.println(nombre);
+          System.out.println(apellido);
+          System.out.println(email);
+          System.out.println(contrasena);
+          System.out.println(pais);
+     }
 
 }
