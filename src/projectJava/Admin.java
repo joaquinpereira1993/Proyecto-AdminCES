8d09b9d25929b3b08222ab73dc90359716e84ba3
joaquinpereira1993 <joaquinpereira40@gmail.com>
2026-06-22 14:17:21 -0300

    Agregado de diagrama UML actualiazado

--- src/projectJava/Admin.java
+++ src/projectJava/Admin.java
@@ -4,7 +4,13 @@ public class Admin extends Usuario {
 
     private String nivelAcceso;
 
-    public Admin(String nombre, String apellido, String email, String pais, String contrasena, String nivelAcceso) {
+    public Admin(String nombre,
+                 String apellido,
+                 String email,
+                 String pais,
+                 String contrasena,
+                 String nivelAcceso)
+    {
         super(nombre, apellido, email, pais, contrasena);
         this.nivelAcceso = NivelAcceso(nivelAcceso);
     }
--- src/projectJava/Tester.java
+++ src/projectJava/Tester.java
@@ -4,7 +4,13 @@ public class Tester extends Usuario {
 
     private String tipoTester;
 
-    public Tester(String nombre, String apellido, String email, String pais, String contrasena, String tipoTester) {
+    public Tester(String nombre,
+                  String apellido,
+                  String email,
+                  String pais,
+                  String contrasena,
+                  String tipoTester)
+    {
         super(nombre, apellido, email, pais, contrasena);
         this.tipoTester = TipoTester(tipoTester);
     }
